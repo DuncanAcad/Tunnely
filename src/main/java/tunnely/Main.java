f9f99b3b521b0541cdb14027cd3a95c7a7f9d232
Duncan <118747708+DuncanAcad@users.noreply.github.com>
2024-10-31 18:37:30 -0300

    Some example tests

--- src/main/java/tunnely/Main.java
+++ src/main/java/tunnely/Main.java
@@ -1,10 +1,35 @@
 package tunnely;
 
+import tunnely.packet.JoinRoomRequestPacket;
 import tunnely.packet.OpenRoomRequestPacket;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        testOpenRoomRequestPacket();
+        testJoinRoomRequestPacket();
+    }
+
+    private static void testOpenRoomRequestPacket() {
+        System.out.println("Testing OpenRoomRequestPacket");
+        OpenRoomRequestPacket p1 = new OpenRoomRequestPacket("Hello", "Test");
+        System.out.println("p1 = " + p1);
+        byte[] p1Bytes = p1.toBytes();
+        System.out.println("p1Bytes = " + Arrays.toString(p1Bytes));
+        OpenRoomRequestPacket p2 = new OpenRoomRequestPacket(p1Bytes);
+        System.out.println("p2 = " + p2);
+        System.out.println();
+    }
+
+    private static void testJoinRoomRequestPacket() {
+        System.out.println("Testing JoinRoomRequestPacket");
+        JoinRoomRequestPacket p1 = new JoinRoomRequestPacket("Hello", "Test");
+        System.out.println("p1 = " + p1);
+        byte[] p1Bytes = p1.toBytes();
+        System.out.println("p1Bytes = " + Arrays.toString(p1Bytes));
+        JoinRoomRequestPacket p2 = new JoinRoomRequestPacket(p1Bytes);
+        System.out.println("p2 = " + p2);
+        System.out.println();
     }
 }
