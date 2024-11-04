7dd6144fa000f62ef80021c67550c8d742ebb04f
Duncan <118747708+DuncanAcad@users.noreply.github.com>
2024-11-04 19:10:04 -0400

    auto format

--- src/main/java/tunnely/packet/CloseConnectionPacket.java
+++ src/main/java/tunnely/packet/CloseConnectionPacket.java
@@ -1,45 +1,46 @@
 package tunnely.packet;
 
 import java.nio.charset.StandardCharsets;
-public class CloseConnectionPacket implements Packet{
+
+public class CloseConnectionPacket implements Packet {
 
     public static Byte ID = 4;// may want to change the id later
 
     private final String message;
 
-    public CloseConnectionPacket(String message){
+    public CloseConnectionPacket(String message) {
         this.message = message;
     }
 
-    public CloseConnectionPacket(byte[] bytes) throws IllegalStateException{
-        if(bytes[0] != ID){
+    public CloseConnectionPacket(byte[] bytes) throws IllegalStateException {
+        if (bytes[0] != ID) {
             throw new IllegalStateException("Invalid Packet ID for " + this.getClass().getSimpleName());
         }
         byte[] bytesMessage = new byte[bytes.length - 1];
-        for(int i = 1; i < bytes.length; i++){
+        for (int i = 1; i < bytes.length; i++) {
             bytesMessage[i - 1] = bytes[i];
         }
         this.message = new String(bytesMessage, StandardCharsets.UTF_8);
     }
 
     @Override
-    public byte[] toBytes(){
+    public byte[] toBytes() {
         byte[] messageBytes = this.message.getBytes(StandardCharsets.UTF_8);
         int packetLength = 1 + messageBytes.length;
         byte[] out = new byte[packetLength];
         out[0] = ID;
-        for(int i = 0; i < messageBytes.length; i++) {
+        for (int i = 0; i < messageBytes.length; i++) {
             out[i + 1] = messageBytes[i];
         }
         return out;
     }
 
+    public byte getId() {
+        return ID;
+    }
+
     @Override
     public String toString() {
         return ("message: " + this.message);//this might need to be changed later
-    }
-
-    public byte getId(){
-        return this.ID;
     }
 }
--- src/main/java/tunnely/packet/NewRoomMemberPacket.java
+++ src/main/java/tunnely/packet/NewRoomMemberPacket.java
@@ -1,31 +1,36 @@
 package tunnely.packet;
 
-public class NewRoomMemberPacket implements Packet{
+public class NewRoomMemberPacket implements Packet {
 
     public static byte ID = 5;//might want to change this later
     private final byte userId;
-    public NewRoomMemberPacket(byte userId){
+
+    public NewRoomMemberPacket(byte userId) {
         this.userId = userId;
     }
-    public NewRoomMemberPacket(byte[] bytes) throws IllegalStateException{
-        if(bytes[0] != ID){
+
+    public NewRoomMemberPacket(byte[] bytes) throws IllegalStateException {
+        if (bytes[0] != ID) {
             throw new IllegalStateException("Invalid Packet ID for " + this.getClass().getSimpleName());
         }
         userId = bytes[1];
     }
+
     @Override
-    public byte[] toBytes(){
+    public byte[] toBytes() {
         byte[] out = new byte[2];
         out[0] = ID;
         out[1] = userId;
         return out;
     }
+
     @Override
     public byte getId() {
         return ID;
     }
+
     @Override
-    public String toString(){
-        return("User ID: " + userId);
+    public String toString() {
+        return ("User ID: " + userId);
     }
 }
