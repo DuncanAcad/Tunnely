4c47015a683d3220be9f2046341728ddab8b44a7
Duncan <118747708+DuncanAcad@users.noreply.github.com>
2024-11-04 19:10:12 -0400

    Add some tests

--- src/main/java/tunnely/Main.java
+++ src/main/java/tunnely/Main.java
@@ -1,6 +1,8 @@
 package tunnely;
 
+import tunnely.packet.CloseConnectionPacket;
 import tunnely.packet.JoinRoomRequestPacket;
+import tunnely.packet.NewRoomMemberPacket;
 import tunnely.packet.OpenRoomRequestPacket;
 
 import java.util.Arrays;
@@ -9,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
         testOpenRoomRequestPacket();
         testJoinRoomRequestPacket();
+        testCloseConnectionPacket();
+        testNewRoomMemberPacket();
     }
 
     private static void testOpenRoomRequestPacket() {
@@ -29,6 +33,28 @@ public class Main {
         byte[] p1Bytes = p1.toBytes();
         System.out.println("p1Bytes = " + Arrays.toString(p1Bytes));
         JoinRoomRequestPacket p2 = new JoinRoomRequestPacket(p1Bytes);
+        System.out.println("p2 = " + p2);
+        System.out.println();
+    }
+
+    private static void testCloseConnectionPacket() {
+        System.out.println("Testing CloseConnectionPacket");
+        CloseConnectionPacket p1 = new CloseConnectionPacket("Hello, I am closing the connection.");
+        System.out.println("p1 = " + p1);
+        byte[] p1Bytes = p1.toBytes();
+        System.out.println("p1Bytes = " + Arrays.toString(p1Bytes));
+        CloseConnectionPacket p2 = new CloseConnectionPacket(p1Bytes);
+        System.out.println("p2 = " + p2);
+        System.out.println();
+    }
+
+    private static void testNewRoomMemberPacket() {
+        System.out.println("Testing NewRoomMemberPacket");
+        NewRoomMemberPacket p1 = new NewRoomMemberPacket((byte) 21);
+        System.out.println("p1 = " + p1);
+        byte[] p1Bytes = p1.toBytes();
+        System.out.println("p1Bytes = " + Arrays.toString(p1Bytes));
+        NewRoomMemberPacket p2 = new NewRoomMemberPacket(p1Bytes);
         System.out.println("p2 = " + p2);
         System.out.println();
     }
