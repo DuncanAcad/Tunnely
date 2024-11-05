2d4da700632624006e260613fa59d5c8a44aaa7d
Geoffrey Norris <0302433n@acadiau.ca>
2024-11-05 11:19:10 -0400

    ConnectionAcceptedPacket

---
+++ src/main/java/tunnely/packet/ConnectionAcceptedPacket.java
@@ -0,0 +1,25 @@
+package tunnely.packet;
+
+public class ConnectionAcceptedPacket implements Packet{
+    public static byte ID = 7;
+
+    public ConnectionAcceptedPacket(){}
+
+    public ConnectionAcceptedPacket(byte[] bytes) throws IllegalStateException{
+        if(bytes[0] != ID){
+            throw new IllegalStateException("Invalid Packet ID for " + this.getClass().getSimpleName());
+        }
+    }
+
+    @Override
+    public byte[] toBytes() {
+        byte[] out = new byte[1];
+        out[0] = ID;
+        return out;
+    }
+
+    @Override
+    public byte getId() {
+        return ID;
+    }
+}
