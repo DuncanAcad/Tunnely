9d8ddd08d5d0c291fc2b9fcfc59c281c1195db21
DuncanAcad <118747708+DuncanAcad@users.noreply.github.com>
2024-11-06 10:11:32 -0400

    Add TunnelyClient and MiddlemanServer

---
+++ src/main/java/tunnely/client/TunnelyClient.java
@@ -0,0 +1,7 @@
+package tunnely.client;
+
+public class TunnelyClient {
+    public static void main(String[] args) {
+        System.out.println("Starting Tunnely Client...");
+    }
+}
---
+++ src/main/java/tunnely/middleman/MiddlemanServer.java
@@ -0,0 +1,7 @@
+package tunnely.middleman;
+
+public class MiddlemanServer {
+    public static void main(String[] args) {
+        System.out.println("Starting Tunnely Middleman Server...");
+    }
+}
