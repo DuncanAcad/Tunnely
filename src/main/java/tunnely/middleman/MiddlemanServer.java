4b95239a3b6414aa538d5245697df214078a0191
DuncanAcad <118747708+DuncanAcad@users.noreply.github.com>
2024-11-07 11:00:04 -0400

    Add rooms field

--- src/main/java/tunnely/middleman/MiddlemanServer.java
+++ src/main/java/tunnely/middleman/MiddlemanServer.java
@@ -1,6 +1,12 @@
 package tunnely.middleman;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MiddlemanServer {
+    private static List<Room> rooms = Collections.synchronizedList(new ArrayList<>());
+
     public static void main(String[] args) {
         System.out.println("Starting Tunnely Middleman Server...");
     }
