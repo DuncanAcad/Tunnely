9aafc27e8bcd30bd4aad72f6df0e76564c57f213
DuncanAcad <118747708+DuncanAcad@users.noreply.github.com>
2024-11-07 10:59:48 -0400

    Create Room.java

---
+++ src/main/java/tunnely/middleman/Room.java
@@ -0,0 +1,34 @@
+package tunnely.middleman;
+
+import java.net.Socket;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Room {
+    private final String name;
+    private final String password;
+
+    private final Socket roomHostConnection;
+    private final Map<Byte, Socket> roomMemberConnections;
+
+    public Room(Socket roomHostConnection, String name, String password) {
+        this.roomHostConnection = roomHostConnection;
+        this.roomMemberConnections = new ConcurrentHashMap<>();
+        this.name = name;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Room)) return false;
+        Room room = (Room) o;
+        return Objects.equals(name, room.name) && Objects.equals(password, room.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, password);
+    }
+}
