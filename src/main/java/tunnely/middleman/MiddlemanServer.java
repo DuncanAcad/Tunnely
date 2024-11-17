a31c5deff5d784093a9ca3470b2b6b7797cae7f4
Duncan <118747708+DuncanAcad@users.noreply.github.com>
2024-11-17 02:30:25 -0400

    Work dump for middleman

--- src/main/java/tunnely/middleman/MiddlemanServer.java
+++ src/main/java/tunnely/middleman/MiddlemanServer.java
@@ -1,5 +1,9 @@
 package tunnely.middleman;
 
+import tunnely.packet.CloseConnectionPacket;
+import tunnely.packet.JoinRoomRequestPacket;
+import tunnely.packet.OpenRoomRequestPacket;
+import tunnely.packet.PacketHelper;
 import tunnely.util.SocketUtil;
 
 import java.io.IOException;
@@ -13,8 +17,8 @@ import java.util.Scanner;
 public class MiddlemanServer {
     private static final List<Room> rooms = Collections.synchronizedList(new ArrayList<>());
     private static ServerSocket serverSocket;
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws IOException {
         System.out.println("Starting Tunnely Middleman Server...");
         // Used System.in instead of Args in case of port already being in use, it will be easier
         // to check the exception in this case, if exception found, enter new port number
@@ -24,23 +28,83 @@ public class MiddlemanServer {
             portNum = new Scanner(System.in).nextInt();
         } while (!SocketUtil.isPortFree(portNum));
 
-        // TODO: Accept connections in a loop and start a thread for each accepted connection running addRoom or joinRoom
+        serverSocket = new ServerSocket(portNum);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            new Thread(() -> tryHandleNewConnection(clientSocket), "Connection Thread: " + clientSocket.getInetAddress()).start();
+        }
+    }
+
+    private static void tryHandleNewConnection(Socket clientSocket) {
+        try {
+            handleNewConnection(clientSocket);
+        } catch (Throwable t) {
+            System.out.println("Failed to handle client connection: " + clientSocket + "\n Error Below:");
+            t.printStackTrace();
+            // Try closing to clean up, if it fails, that's alright
+            if (!clientSocket.isClosed()) try {
+                clientSocket.close();
+            } catch (IOException ignored) {
+            }
+        }
+    }
+
+    private static void handleNewConnection(Socket clientSocket) throws IOException {
+        byte[] bytes = PacketHelper.receivePacketBytes(clientSocket);
+        if (bytes == null) {
+            clientSocket.close();
+            return;
+        }
+        byte packetId = bytes[0];
+        try {
+            if (packetId == OpenRoomRequestPacket.ID) {
+                OpenRoomRequestPacket orrp = new OpenRoomRequestPacket(bytes);
+                addRoom(orrp.getName(), orrp.getPass(), clientSocket);
+            } else if (packetId == JoinRoomRequestPacket.ID) {
+                JoinRoomRequestPacket jrrp = new JoinRoomRequestPacket(bytes);
+                joinRoom(jrrp.getName(), jrrp.getPass(), clientSocket);
+            } else {
+                PacketHelper.sendPacket(clientSocket, new CloseConnectionPacket("Invalid packet received! Should be an OpenRoomRequestPacket or a JoinRoomRequestPacket."));
+                clientSocket.close();
+            }
+        } catch (IllegalStateException /*Thrown by packet constructors*/ e) {
+            PacketHelper.sendPacket(clientSocket, new CloseConnectionPacket("Invalid packet received! Correct packet ID, but invalid data format."));
+            clientSocket.close();
+        }
     }
 
     private static boolean roomExists(String name) {
-        throw new RuntimeException("Not implemented yet!"); // TODO
+        return getRoom(name) != null;
     }
 
     private static boolean hasCorrectRoomPassword(String name, String password) {
-        throw new RuntimeException("Not implemented yet!"); // TODO
+        return getRoom(name).getPassword().equals(password);
     }
 
-    private static void addRoom(String name, String password, Socket roomHost) {
-        throw new RuntimeException("Not implemented yet!"); // TODO
+    private static void addRoom(String name, String password, Socket hostSocket) throws IOException {
+        if (roomExists(name)) {
+            PacketHelper.sendPacket(hostSocket, new CloseConnectionPacket("Cannot open room: room already exists."));
+            hostSocket.close();
+            return;
+        }
+        Room room = new Room(hostSocket, name, password);
+        rooms.add(room);
     }
 
-    private static void joinRoom(String name, String password, Socket roomMember) {
-        throw new RuntimeException("Not implemented yet!"); // TODO
+    private static void joinRoom(String name, String password, Socket clientSocket) throws IOException {
+        if (!roomExists(name)) {
+            PacketHelper.sendPacket(clientSocket, new CloseConnectionPacket("Cannot join room: room does not exist."));
+            clientSocket.close();
+            return;
+        }
+        if (!hasCorrectRoomPassword(name, password)) {
+            PacketHelper.sendPacket(clientSocket, new CloseConnectionPacket("Cannot join room: incorrect password."));
+            clientSocket.close();
+            return;
+        }
+        Room room = getRoom(name);
+        room.join(clientSocket);
     }
 
     // we should have the ability for the server to close rooms should we not?
@@ -48,4 +112,9 @@ public class MiddlemanServer {
         throw new RuntimeException("Not implemented yet!"); // TODO
     }
 
+    private static Room getRoom(String name) {
+        synchronized (rooms) {
+            return rooms.stream().filter(room -> room.getName().equals(name)).findFirst().orElse(null);
+        }
+    }
 }
--- src/main/java/tunnely/middleman/Room.java
+++ src/main/java/tunnely/middleman/Room.java
@@ -1,11 +1,20 @@
 package tunnely.middleman;
 
+import tunnely.packet.CloseConnectionPacket;
+import tunnely.packet.EvalMemberPacket;
+import tunnely.packet.NewRoomMemberPacket;
+import tunnely.packet.PacketHelper;
+
+import java.io.IOException;
 import java.net.Socket;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Room {
+    private boolean closed = false;
+
     private final String name;
     private final String password;
 
@@ -30,5 +39,83 @@ public class Room {
         if (!(o instanceof Room)) return false;
         Room room = (Room) o;
         return Objects.equals(name, room.name) && Objects.equals(password, room.password);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public synchronized void join(Socket roomMember) throws IOException {
+        byte newId = -1;
+        Set<Byte> currentBytes = roomMemberConnections.keySet();
+        for (int i = 0; i <= Byte.MAX_VALUE; i++) {
+            if (currentBytes.contains((byte) i)) continue;
+            newId = (byte) i;
+            break;
+        }
+        if (newId == -1) {
+            PacketHelper.sendPacket(roomMember, new CloseConnectionPacket("Failed to join room: no user IDs available."));
+            roomMember.close();
+            return;
+        }
+        PacketHelper.sendPacket(roomHostConnection, new NewRoomMemberPacket(newId));
+        byte[] bytes = PacketHelper.receivePacketBytes(roomHostConnection);
+        if (bytes == null) {
+            // Uh oh, unexpected response
+            closeRoom("Closing room: improper/invalid/no response.");
+            return;
+        }
+        if (bytes[0] != EvalMemberPacket.ID) {
+            // Uh oh, unexpected response
+            return;
+        }
+        EvalMemberPacket emp;
+        try {
+            emp = new EvalMemberPacket(bytes);
+        } catch (Throwable t) {
+            // Uh oh
+            closeRoom("Closing room: invalid packet as response.");
+            return;
+        }
+        if (!emp.isAccepted()) {
+            PacketHelper.sendPacket(roomMember, new CloseConnectionPacket("Failed to join room: room host rejected join request."
+                    + (emp.getMessage().isEmpty() ? "" : (" Room host gave the following reason: " + emp.getMessage()))));
+            roomMember.close();
+            return;
+        }
+        // Finally, add the room member to our connections map
+        roomMemberConnections.put(newId, roomMember);
+    }
+
+    private void closeRoom(String message) {
+        try {
+            PacketHelper.sendPacket(roomHostConnection, new CloseConnectionPacket(message));
+        } catch (Throwable ignored) {
+        }
+        this.close();
+    }
+
+    private synchronized void close() {
+        // Kill it all and mark as closed!
+        // Marking as closed will let the middleman server check isClosed and see it can remove it from its rooms list.
+        closed = true;
+        try {
+            roomHostConnection.close();
+        } catch (Throwable ignored) {
+        }
+        roomMemberConnections.forEach((aByte, socket) -> {
+            try {
+                socket.close();
+            } catch (Throwable ingored) {
+            }
+        });
+    }
+
+    public boolean isClosed() {
+        return closed; // TODO use this method in MiddlemanServer to remove removes, make a checkClosedRooms method to remove them and use checkClosedRooms at the start of every join/create request
     }
 }
